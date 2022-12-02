253b141fbde0a06ddcb375b1730e58628ed34d1a
SATTDN22.03.06 Duong Tuan Minh <SATTDN22.03.06@logigear.com>
2022-12-02 08:48:05 +0700

    create test case 093

---
+++ src/test/java/com/tadashboard/testcases/DA_DP/TC093.java
@@ -0,0 +1,25 @@
+package com.tadashboard.testcases.DA_DP;
+
+import com.tadashboard.common.utilities.logs.Log;
+import com.tadashboard.pageObjects.TADashBoard.LoginPage;
+import com.tadashboard.pageObjects.TADashBoard.ProfilePage;
+import com.tadashboard.testcases.BaseTestSetUp;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC093 extends BaseTestSetUp {
+    @Test(description = "Verify that user is able to remove filter criteria successfully")
+    public void TC093_RemoveFilterCriteria() throws InterruptedException {
+        LoginPage loginPage = new LoginPage();
+        Log.info("Login with valid account");
+        loginPage.login("test", "test");
+        ProfilePage profilePage = new ProfilePage();
+        Log.info("Go to filter field");
+        profilePage.goToFilterField("test");
+        Log.info("Add new filter");
+        profilePage.addNewFilter();
+        Log.info("Remove filter");
+        profilePage.removeFilter();
+        Assert.assertFalse(profilePage.isFilterDisplayed());
+    }
+}
